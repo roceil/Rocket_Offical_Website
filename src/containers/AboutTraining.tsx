ed54c2a481d5e0aa74750b04c6ae78f3bcea17de
roceil <a0978006326@gmail.com>
2023-05-22 01:18:40 +0800

    [Style] adjust aboutTraning bgColor

--- src/containers/AboutTraining.tsx
+++ src/containers/AboutTraining.tsx
@@ -196,7 +196,7 @@ function AboutTraining() {
   };
 
   return (
-    <section className="bg-neutral-100">
+    <section className="bg-[#F8F9FD]">
       <div className="relative pb-[76px] pt-[30px] lg:pt-[38px]">
         <h2 className="h2 absolute left-1/2 top-0 -translate-x-1/2 -translate-y-1/2 text-center">
           關於培訓
@@ -630,7 +630,7 @@ function AboutTraining() {
 
         {/* 教練團 */}
         <div
-          className=" flex justify-center bg-neutral-100 pb-[60px] pt-[52px]"
+          className=" flex justify-center bg-[#F8F9FD] pb-[60px] pt-[52px]"
           ref={coachsRef}
         >
           <div className="container max-w-[1076px] lg:px-0">
